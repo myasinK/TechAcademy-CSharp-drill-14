333f4758b89a94a6b56243ca31fbe9d249df6f11
myasinK <yasinul@gmail.com>
2018-08-15 19:55:20 -0700

    Finish third drill on classes

---
+++ IntroToClassesPart3/Method.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClassesPart3
+{
+    public class Method
+    {
+        public int Add10(int num)
+        {
+            return num + 10;
+        }
+        public int Add10(decimal num)
+        {
+            return (int)num / 10; 
+        }
+        public int Add10(string str)
+        {
+            try
+            {
+                int str_try = Convert.ToInt32( str );
+                return str_try + 10;
+            }
+            catch
+            {
+                Console.WriteLine("Error: Conversion not possible");
+                return 0;
+            }
+            
+        }
+    }
+}
---
+++ IntroToClassesPart3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClassesPart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method method = new Method();
+            // trigger first method with int
+            int N = 97;
+            Console.WriteLine( method.Add10( N ) );
+            // trigger second method with decimal param
+            decimal N_dec = Convert.ToDecimal( N );
+            Console.WriteLine( method.Add10( N_dec ) );
+            // trigger third method with string param
+            string str = "97a";
+            int str_int = method.Add10( str );
+            Console.WriteLine( str_int );
+        }
+    }
+}
